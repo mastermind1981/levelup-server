57e9cf10eb0d16452d9f2dd2ff49feeee4d24bcd
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-14 14:56:38 -0500

    Added an endpoint for updating user levels.

--- server.js
+++ server.js
@@ -4,7 +4,32 @@ var application = express();
 
 application.get("/", function(request, response)
 {
-    response.send("Hello World!");
+    response.send("Hello, and welcome to LevelUp!");
 });
 
+application.get("/:name", function(request, response)
+{
+    var name = request.params.name;
+    
+    if(!users[name]) {
+        users[name] = {
+            lvl: 1,
+            xp: 0
+        }
+    }
+
+    var user = users[name];
+
+    user.xp += 1;
+    if(user.xp > 10)
+    {
+        user.lvl += 1;
+        user.xp = 0;
+    }
+
+    response.send(name + ": xp is " + user.xp + ", lvl is " + user.lvl);
+})
+
 application.listen(8080);
+
+var users = {};
